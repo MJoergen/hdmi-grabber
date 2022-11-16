70fb92cf6ee06ba1a53d11a5fe7e85718d181954
Michael Jørgensen <mfj@silicom.dk>
2022-11-16 17:29:03 +0100

    top-level: Explicitly instantiate I/O buffers
    They are needed for the differential pair signals

--- src/Zybo-Z7-Master.vhd
+++ src/Zybo-Z7-Master.vhd
@@ -1,6 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library unisim;
+use unisim.vcomponents.all;
+
 entity Zybo_Z7_Master is
    port (
       sysclk        : in    std_logic;
@@ -27,14 +30,15 @@ end entity Zybo_Z7_Master;
 
 architecture synthesis of Zybo_Z7_Master is
 
+   signal hdmi_rx_clk : std_logic;
+   signal hdmi_rx_dat : std_logic_vector(2 downto 0);
+   signal hdmi_tx_clk : std_logic;
+   signal hdmi_tx_dat : std_logic_vector(2 downto 0);
+
 begin
 
    -- Connect input to output
    hdmi_tx_hpd   <= hdmi_rx_hpd;
-   hdmi_tx_clk_n <= hdmi_rx_clk_n;
-   hdmi_tx_clk_p <= hdmi_rx_clk_p;
-   hdmi_tx_n     <= hdmi_rx_n;
-   hdmi_tx_p     <= hdmi_rx_p;
 
    -- Tri-state bidirectional ports
    hdmi_rx_scl   <= 'Z';
@@ -43,6 +47,41 @@ begin
    hdmi_tx_scl   <= 'Z';
    hdmi_tx_sda   <= 'Z';
    hdmi_tx_cec   <= 'Z';
+
+   ibufds_clk_inst : ibufds
+      port map (
+         i  => hdmi_rx_clk_p,
+         ib => hdmi_rx_clk_n,
+         o  => hdmi_rx_clk
+      ); -- ibufds_clk_inst
+
+   gen_ibuf : for i in 0 to 2 generate
+      ibufds_dat_inst : ibufds
+         port map (
+            i  => hdmi_rx_p(i),
+            ib => hdmi_rx_n(i),
+            o  => hdmi_rx_dat(i)
+         ); -- ibufds_dat_inst
+      end generate gen_ibuf;
+
+   hdmi_tx_clk <= hdmi_rx_clk;
+   hdmi_tx_dat <= hdmi_rx_dat;
+
+   obufds_clk_inst : obufds
+      port map (
+         i  => hdmi_tx_clk,
+         o  => hdmi_tx_clk_p,
+         ob => hdmi_tx_clk_n
+      ); -- obufds_clk_inst
+
+   gen_obuf : for i in 0 to 2 generate
+      obufds_dat_inst : obufds
+         port map (
+            i  => hdmi_tx_dat(i),
+            o  => hdmi_tx_p(i),
+            ob => hdmi_tx_n(i)
+         ); -- obufds_dat_inst
+      end generate gen_obuf;
 
 end architecture synthesis;
 

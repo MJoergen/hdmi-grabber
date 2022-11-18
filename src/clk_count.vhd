bff7280052c796c1b20d950748ce4571f4484e5b
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2022-11-18 07:43:24 +0100

    Refacror clock counter

--- src/clk_count.vhd
+++ src/clk_count.vhd
@@ -2,76 +2,97 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std_unsigned.all;
 
+library xpm;
+use xpm.vcomponents.all;
+
 entity clk_count is
-   generic (G_REF_CNT_LEN : integer := 15);
+   generic (
+      G_COUNTER_SIZE         : integer := 28,
+      G_INPUT_CLOCK_SPPED_HZ : integer := 200_000_000
+   );
    port (
-      ref_clk_i    : in  std_logic;
-      clk0_clk_i   : in  std_logic;
-      clk0_count_o : out std_logic_vector(15 downto 0)
+      clk_i     : in  std_logic;
+      count_o   : out std_logic_vector(G_COUNTER_SIZE-1 downto 0);
+      obj_clk_i : in  std_logic
    );
 end entity clk_count;
 
 architecture synthesis of clk_count is
 
-   constant C_ALL_ONES     : std_logic_vector(G_REF_CNT_LEN-1 downto 0) := (others => '1');
-   signal   ref_count_i    : std_logic_vector(G_REF_CNT_LEN-1 downto 0) := (others => '0');
-   signal   ref_sample_out : std_logic                                  := '0';
-   signal   ref_sample_in  : std_logic                                  := '0';
+   -- clk domain
+   signal clk_counter : std_logic_vector(G_COUNTER_SIZE-1 downto 0);
+   signal clk_pps     : std_logic;
 
-   signal clk0_count_i     : std_logic_vector(15 downto 0) := (others => '0');
-   signal clk0_count_latch : std_logic_vector(15 downto 0) := (others => '0');
-   signal clk0_count_sync  : std_logic_vector(15 downto 0) := (others => '0');
-   signal clk0_count_ref   : std_logic_vector(15 downto 0) := (others => '0');
-   signal clk0_count_rx    : std_logic_vector(15 downto 0) := (others => '0');
-   signal clk0_sample_i    : std_logic                     := '0';
+   -- obj_clk domain
+   signal obj_counter : std_logic_vector(G_COUNTER_SIZE-1 downto 0);
+   signal obj_latch   : std_logic_vector(G_COUNTER_SIZE-1 downto 0);
+   signal obj_pps     : std_logic;
 
 begin
 
-   pci_proc : process(ref_clk_i)
+   --------------
+   -- clk domain
+   --------------
+
+   clk_counter_p : process (clk_i)
    begin
-      if rising_edge(ref_clk_i) then
-         ref_count_i <= ref_count_i + 1;
-         -- when rolling over on the reference counter, send out the last values
-         -- and indicate to the other counters that their values
-         -- shall be latched and sent to the reference clock domain:
-         if ref_count_i = c_all_ones then
-            ref_sample_out <= '1';
+      if rising_edge(clk_i) then
+         if clk_counter = G_INPUT_CLOCK_SPPED_HZ - 1 then
+            clk_counter <= (others => '0');
+            clk_pps     <= '1';
          else
-            ref_sample_out <= '0';
-         end if;
-
-         if ref_sample_out = '1' then
-            clk0_count_ref <= clk0_count_sync;
-         else
-            clk0_count_ref <= clk0_count_ref;
-         end if;
-
-         ref_sample_in <= ref_sample_out;
-
-      end if;
-   end process pci_proc;
-
-   clk0_count_o <= clk0_count_ref;
-
-   clk0_count_rx <= clk0_count_latch;
-
-   clk0_sample_conv : entity work.pulse_conv
-      port map(clk_src => ref_clk, en_src => ref_sample_in, clk_dst => clk0_clk, en_dst => clk0_sample_i);
-
-   clk0_proc : process(clk0_clk_i)
-   begin
-      if rising_edge(clk0_clk_i) then
-         clk0_count_i <= clk0_count_i + 1;
-         if clk0_sample_i = '1' then
-            clk0_count_latch <= clk0_count_i;  -- sample and hold counter value
-            clk0_count_i     <= (others => '0');
+            clk_counter <= clk_counter + 1;
+            clk_pps     <= '0';
          end if;
       end if;
-   end process clk0_proc;
+   end process clk_counter_p;
 
-   cdc_sync_bundle_clk0_count : entity work.cdc_sync_bundle(synth)
-      generic map( G_WIDTH      => 16, G_SYNC_DEPTH => 2  )
-      port map ( clk => ref_clk, i => clk0_count_rx, o => clk0_count_sync );
+
+   -------------------
+   -- CDC
+   -------------------
+
+   xpm_cdc_pulse_inst : xpm_cdc_pulse
+      generic map (
+         RST_USED     => 0,
+         DEST_SYNC_FF => 2
+      )
+      port map (
+         src_clk    => clk_i,
+         src_rst    => '0',
+         src_pulse  => clk_pps,
+         dest_clk   => obj_clk_i,
+         dest_rst   => '0',
+         dest_pulse => obj_pps
+      ); -- xpm_cdc_pulse_inst
+
+   xpm_cdc_array_single_inst : xpm_cdc_array_single
+      generic map (
+         DEST_SYNC_FF => 2,
+         WIDTH        => G_COUNTER_SIZE
+      )
+      port map (
+         src_clk  => obj_clk_i,
+         src_in   => obj_latch,
+         dest_clk => clk_i,
+         dest_out => count_o
+      ); -- xpm_cdc_array_single_inst
+
+
+   -------------------
+   -- obj_clk domain
+   -------------------
+
+   obj_counter_p : process (obj_clk_i)
+   begin
+      if rising_edge(obj_clk_i) then
+         obj_counter <= obj_counter + 1;
+         if obj_pps = '1' then
+            obj_latch <= obj_counter;
+            obj_counter <= (others => '0');
+         end if;
+      end if;
+   end process obj_counter_p;
 
 end architecture synthesis;
 
